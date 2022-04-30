f1b3d953a3be9e9f042b81c71f8b94c06e6ab7b2
yeonui-0626 <73818604+yeonui-0626@users.noreply.github.com>
2022-04-30 22:59:42 +0900

    boj 수학

--- Baekjoon/BJ_1934_최소공배수.java
+++ Baekjoon/BJ_1934_최소공배수.java
@@ -4,11 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
-/*
 
-유클리드 호제법
-
- */
 public class BJ_1934_최소공배수 {
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
